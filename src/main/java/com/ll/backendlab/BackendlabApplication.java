eb580de1109175d08dc3fc7b78255c8cd3bd8ff4
gorhkd <gorhkd@gmail.com>
2026-08-04 10:43:34 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/ll/backendlab/BackendlabApplication.java
@@ -0,0 +1,13 @@
+package com.ll.backendlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendlabApplication.class, args);
+	}
+
+}
